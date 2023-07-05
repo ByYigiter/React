1855b7058036d6f637d9e923c190b41b30845306
Yigit Er <mr.yigiter222@gmail.com>
2023-07-05 08:54:27 +0300

    project 01

---
+++ src/components/01-hello-world/01-hello-world.js
@@ -0,0 +1,15 @@
+import Comp from "../02-comp/02-comp";
+
+
+const HelloWorld = () => {
+
+   
+
+    return (
+        <h1>Hello New Big World <Comp></Comp></h1>
+        
+        
+    );
+};
+
+export default HelloWorld;
