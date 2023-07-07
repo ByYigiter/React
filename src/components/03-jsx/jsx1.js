c0db33c15f075fbfc0ba9830d3f7bc761d4f25f3
Yigit Er <mr.yigiter222@gmail.com>
2023-07-07 10:18:11 +0300

    project 03

---
+++ src/components/03-jsx/jsx1.js
@@ -0,0 +1,14 @@
+const Jsx1 = () => {
+
+    const name = "Ali Gel";
+    const isAdmin = true;
+
+    return (
+        <div>
+        <p>All Elements should be under 1 root</p>
+        <div>{name}</div>
+        </div>
+    )
+}
+
+export default Jsx1;
